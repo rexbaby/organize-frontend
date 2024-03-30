01dbbea143b09ddfa783ce21800d2c88dc0db01c
Emma <>
2024-03-30 15:13:19 +0800

    create core module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CoreComponent } from './core/core.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CoreComponent },
+  { path: '**', component: NotFoundComponent },
+];
