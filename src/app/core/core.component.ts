9246920e41cf7c082ea4ff52964456908a71f8d6
Emma <>
2024-04-26 12:15:23 +0800

    update menu

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { IPermission } from '../service/permission/permission';
+import { PermissionService } from '../service/permission/permission.service';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-core',
@@ -19,17 +21,63 @@ import { IPermission } from '../service/permission/permission';
   templateUrl: './core.component.html',
   styleUrl: './core.component.scss',
 })
-export class CoreComponent implements OnInit {
+export class CoreComponent implements OnInit, OnDestroy {
+  subscription: Subscription | null = null;
   tabs: IPermission[] = [];
   tabNowIndex = -1;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private permissionService: PermissionService
+  ) {}
 
   ngOnInit(): void {
+    this.contentSwitch();
+    const permission$ = this.permissionService.isPermissionIn();
+    if (!permission$) return;
+    this.subscription = permission$.subscribe((p) => {
+      this.tabCheck(p);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  tabCatchIndex(p: IPermission) {
+    return this.tabs.map((t) => t.title + t.suffix).indexOf(p.title + p.suffix);
+  }
+
+  tabCheck(p: IPermission) {
+    const i = this.tabCatchIndex(p);
+    if (i !== -1) {
+      this.tabNowIndex = i;
+      this.contentSwitch();
+      return;
+    }
+    this.tabs.push(p);
+    this.tabNowIndex = this.tabs.length - 1;
+    this.contentSwitch();
+  }
+
+  tabClick(p: IPermission) {
+    const i = this.tabCatchIndex(p);
+    this.tabNowIndex = i;
+    this.contentSwitch();
+  }
+
+  tabDel(p: IPermission) {
+    const i = this.tabCatchIndex(p);
+    if (i === -1) return;
+    if (this.tabNowIndex === i) this.tabNowIndex--;
+    this.tabs.splice(i, 1);
     this.contentSwitch();
   }
 
   contentSwitch() {
+    console.log(44,this.tabs)
     if (!this.tabs.length) {
       this.router.navigate(['/index']);
       return;
@@ -38,25 +86,5 @@ export class CoreComponent implements OnInit {
     this.router.navigate([
       `/${tabNow.url}${tabNow.suffix ? '/' + tabNow.suffix : ''}`,
     ]);
-  }
-
-  tabClick(p: IPermission) {
-    const i = this.tabs
-      .map((t) => t.title + t.suffix)
-      .indexOf(p.title + p.suffix);
-
-    this.tabNowIndex = i;
-    this.contentSwitch();
-  }
-
-  tabDel(p: IPermission) {
-    const i = this.tabs
-      .map((t) => t.title + t.suffix)
-      .indexOf(p.title + p.suffix);
-
-    if (i === -1) return;
-    if (this.tabNowIndex === i) this.tabNowIndex--;
-    this.tabs.splice(i, 1);
-    this.contentSwitch();
   }
 }
--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IPermission } from '../../service/permission/permission';
+import { PermissionService } from '../../service/permission/permission.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
-  menus = [{ tag: 'menu01' }, { tag: 'menu02' }];
+  menus: IPermission[] = [
+    { url: '/area', title: 'Area', suffix: '' },
+    { url: '/person', title: 'Person', suffix: '' },
+  ];
+  menuNowIndex = -1;
+
+  constructor(private permissionService: PermissionService) {}
+
+  menuClick(i: number) {
+    if (i === this.menuNowIndex) return;
+    this.menuNowIndex = i;
+    this.permissionService.nextPermission(this.menus[i]);
+  }
 }
